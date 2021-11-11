b0664148bf3701d8ec919da9e238bea84cf6b259
DevTranVanNguyen <nobitavvvvvv@gmail.com>
2021-11-11 16:07:36 +0700

    update

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({Key? key}) : super(key: key);
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          child: _previewImageUrl != null
+              ? const Text(
+                  "No Location Chosen",
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.location_on),
+            label: const Text('Current Location'),
+        ),
+        ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.map),
+          label: const Text('Select on Map'),
+          style: ElevatedButton.styleFrom(
+            onPrimary: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
